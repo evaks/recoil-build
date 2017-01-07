616d387dd5a11821cec02887ccdc8ef8617ae794
Eduard Vaks <eddie.vaks@aviatnet.com>
2017-01-07 16:40:38 +1300

    remove cr lint-fix

--- bin/lint-fix.sh
+++ bin/lint-fix.sh
@@ -5,5 +5,9 @@ DIR=`dirname $EXE`
 
 cd $DIR/..
 fixjsstyle --disable 0110,0120 `find recoil/src/ -name "*.js" -and -not -name "*_test.js" `
+
+#on windows this adds cr
+which cygpath && find recoil/src/ -name "*.js" -and -not -name "*_test.js" -exec sed -i 's/\r//' {} \; 
+#sed 's/\r/<replace with text>/'
 #echo $DIR
 #gjslint --disable 0110,0120 `ls *.js | grep -v _test.js`
